c34699dcd970b8920e0b33af016a445718099659
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-09-20 19:36:42 +0300

    StudentController index&Details views

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        DemoDbContext context = new DemoDbContext();    
+        public IActionResult Index()
+        {
+            var students = context.Students.Include(s => s.Department).ToList();
+            return View(students);
+        }
+
+       public IActionResult Details(int id)
+        {
+            var student = context.Students.Include(s => s.Department).FirstOrDefault(s => s.ID == id);
+            return View(student);
+        }
+    }
+}
