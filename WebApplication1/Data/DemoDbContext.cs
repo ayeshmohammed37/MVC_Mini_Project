e7420c31768a3c8a5d16dafff424aaf896535384
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-09-20 18:38:39 +0300

    Data/DbContext

---
+++ WebApplication1/Data/DemoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DemoDbContext: DbContext
+    {
+
+        public DemoDbContext(): base()
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StuCrsRes> StuCrsRess { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Demo;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
