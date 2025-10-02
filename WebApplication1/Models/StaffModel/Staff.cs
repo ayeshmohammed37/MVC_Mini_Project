b4ed42fbcb4f1c2d8acb73c5318d358fcf0cdc30
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-10-02 07:52:17 +0300

    update Models Properties

--- WebApplication1/Models/StaffModel/Staff.cs
+++ WebApplication1/Models/StaffModel/Staff.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Models.StaffModel
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
+        public string? MiddleName { get; set; }
         public string LastName { get; set; }
         public string Code { get; set; }
         public string? Img { get; set; }
@@ -22,16 +22,16 @@ namespace WebApplication1.Models.StaffModel
         public Gender Gender { get; set; }
         public string Nationality { get; set; }
         public DateTime BirthDate { get; set; }
-        public string BirthPlace { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
-        public string HomeTele { get; set; }
+        public string? BirthPlace { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public string? HomeTele { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
-        public string Fax { get; set; }
-        public string MailBox { get; set; }
+        public string? Fax { get; set; }
+        public string? MailBox { get; set; }
         public String Degree { get; set; }
-        public string OfficeLocation { get; set; }
+        public string? OfficeLocation { get; set; }
 
         //Navigation Properties
         public virtual List<Student>? Students { get; set; } = new List<Student>();
--- WebApplication1/Models/StudentModel/Student.cs
+++ WebApplication1/Models/StudentModel/Student.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Models.StudentModel
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
+        public string? MiddleName { get; set; }
         public string LastName { get; set; }
         public string Code { get; set; }
         public int Level { get; set; }
@@ -23,14 +23,14 @@ namespace WebApplication1.Models.StudentModel
         public Gender Gender { get; set; }
         public string Nationality { get; set; }
         public DateTime BirthDate { get; set; }
-        public string BirthPlace { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
-        public string HomeTele { get; set; }
+        public string? BirthPlace { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public string? HomeTele { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
-        public string Fax { get; set; }
-        public string MailBox { get; set; }
+        public string? Fax { get; set; }
+        public string? MailBox { get; set; }
 
 
         //Navigation Properties
