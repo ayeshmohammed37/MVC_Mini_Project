773cf39ae529962c1a3fd60224f8cac9ff6b6c1c
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-10-03 08:29:55 +0300

    working on student details page

---
+++ WebApplication1/Controllers/StudentPortalController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models.StudentModel;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentPortalController : Controller
+    {
+
+        DemoContext context = new DemoContext();
+        public IActionResult Index()
+        {
+            var students = context.Students.Include(std => std.Department).ToList();
+            return View(students);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Student std = context.Students
+                .FirstOrDefault(std => std.ID == id);
+            //context.Entry<Student>(std).Reference(s => s.Department).Load();
+            //context.Entry<Student>(std).Reference(s => s.Advisor).Load();
+            //context.Entry<Student>(std).Collection(s => s.CourseGrades).Load();
+
+            return View(std);
+        }
+    }
+}
