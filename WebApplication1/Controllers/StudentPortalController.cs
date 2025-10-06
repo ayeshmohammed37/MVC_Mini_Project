c850ba9859003d4f0a33d1fc839e3e1859faa119
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-10-06 08:48:59 +0300

    Student Details

--- WebApplication1/Controllers/StudentPortalController.cs
+++ WebApplication1/Controllers/StudentPortalController.cs
@@ -15,10 +15,15 @@ namespace WebApplication1.Controllers
             return View(students);
         }
 
+
+        
         public IActionResult Details(int id)
         {
+            Student std = context.Students.FirstOrDefault(st => st.ID == id);
+            context.Entry<Student>(std).Reference(st => st.Department).Load();
+            context.Entry<Student>(std).Reference(st => st.Advisor).Load();
 
-            return View();
+            return View(std);
         } 
 
     }
